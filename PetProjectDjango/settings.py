dc913ac61199cfdac88c9dce99d84531fd0b5f65
darten360 <darten360@gmail.com>
2022-12-03 14:11:58 +0100

    Removed insecure Django Secret Key

--- PetProjectDjango/settings.py
+++ PetProjectDjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANFO_SECRET_KEY", "django-insecure-$v-zmj1tun3pbr1c7851ov_oxlu3hrd-zu!j^w(ht(cvbw7ch&")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
